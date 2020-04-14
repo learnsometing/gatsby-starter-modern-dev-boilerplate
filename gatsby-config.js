896ee43633751440712461ceb58c1036242cf099
Brian Monaccio <brianmonaccio@gmail.com>
2020-04-13 20:10:59 -0400

    Adds eslint with typescript/prettier support and adds stylelint with prettier support

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-starter-ts-and-tests`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
